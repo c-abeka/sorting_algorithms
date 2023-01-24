bd5503c59e551f12c46cf913c620586e7c7f0ec7
c-abeka <havelstech@gmail.com>
2023-01-25 01:13:34 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -35,7 +35,7 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap(array[i], array[1 + 1]);
+				swap(&array[i], &array[1 + 1]);
 				while (array && j < size)
 				{
 					if (i > 0)
@@ -44,7 +44,7 @@ void bubble_sort(int *array, size_t size)
 					j++;
 				}
 				printf("\n");
-				bubble = false
+				bubble = false;
 			}
 		}
 		len--;
